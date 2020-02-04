c9ad4575628bd3d0831c518c08a9eaeacee17ffa
leduclinh <leduclinh.7141@gmail.com>
2020-02-04 14:43:19 +0900

    feat: update

--- 005-basic-navigation/01-start/lib/page2.dart
+++ 005-basic-navigation/01-start/lib/page2.dart
@@ -3,13 +3,33 @@ import 'package:flutter/material.dart';
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Page 2',
-              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+    return WillPopScope( 
+      onWillPop: () async {
+        _popNavigationWithResult(context, 'from_back');
+        return false;
+      },
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          _popNavigationWithResult(context, 'from_button');
+        }),
+        body: Container( 
+          child: Center( 
+            child: Text('Page 2', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+          )
         ),
       ),
     );
+    // return Scaffold(
+    //   body: Container(
+    //     child: Center(
+    //       child: Text('Page 2',
+    //           style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+    //     ),
+    //   ),
+    // );
+  }
+
+  void _popNavigationWithResult(BuildContext context, dynamic result) {
+    Navigator.pop(context, result);
   }
 }
